83b63ef4906634a3339143df7592da831356e654
elevalgue <elena.valenzuela.guerrero@gmail.com>
2020-09-29 20:56:24 +0200

    Add the watch's second hand movement functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const secondHand = document.querySelector('.second-hand');
+
+function setDate() {
+  const now = new Date();
+  //seconds
+  const seconds = now.getSeconds();
+  const secondsDegrees = (seconds / 60) * 360;
+  secondHand.getElementsByClassName.transform = `rotate(${secondsDegrees}deg)`;
+
+  console.log(seconds);
+}
+
+setInterval(setDate, 1000);
